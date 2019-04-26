e1a31d1684da04a63fa070a2a9db348de524d519
Juan Rodriguez <juanjorog@users.noreply.github.com>
2019-04-26 11:55:40 -0500

    Add files via upload

---
+++ codecept.conf.js
@@ -0,0 +1,56 @@
+const steps= require("./src/features");
+
+exports.config = {
+  output: './report',
+  helpers: {
+    Puppeteer: {
+      url: 'https://sbprol3.somosbelcorp.com/',
+     //fullPageScreenshots: true,
+      windowSize: "1350x720",
+      show:true,
+      chrome: {
+        args: [
+              //'--use-fake-ui-for-media-stream',
+              //'--disable-web-security',
+              //'--use-fake-device-for-media-stream',
+              //'--allow-file-access-from-files',
+              //'--allow-running-insecure-content',
+              '--window-size=1920,1080'
+        ]
+    }
+      
+    },
+    Mochawesome: {  
+      uniqueScreenshotNames: true
+                }
+  },
+  include: {},
+  mocha: {
+    reporterOptions: {
+      reportDir: "./report/",
+      reportFilename: 'BusquedaProducto',
+      reportTitle: 'Somos-Belcorp',
+      autoOpen:true,
+      ts:'',
+    }
+  },
+  bootstrap: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './src/features/**/**/*.feature',
+    steps: [
+      ...steps
+         ]
+  },
+  plugins: {
+    allure: {},
+    screenshotOnFail: {
+      enabled: true
+    }
+  },
+  tests: './*_test.js',
+  name: 'Portal-SomosBelcorp'
+}
+
+//console.log(module.exports.config)
